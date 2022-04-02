89af63fb85ec3de58a40b84c777823f31a49812a
thaianealm <cont.thaiane@gmail.com>
2022-04-02 09:09:59 -0300

    commit inicial

--- js/homepage.js
+++ js/homepage.js
@@ -1,3 +1,3 @@
 function startTheGame(){
-    window.location.href = "file:///C:/workspace/duck-game-html-css-javascript/duckgame.html";
+    window.location.href = "duck-game-html-css-javascript/duckgame.html";
 }
